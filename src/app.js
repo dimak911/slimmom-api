157ff1f0e48510076b3627a4093989890d04e2cb
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-19 10:27:01 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // use routers here
 app.use("/api/diary", usersProductsRouter);
