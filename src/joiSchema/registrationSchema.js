9ea80fd613de97a1a90f905e58e675735f62a579
Yevhenii Kobets <eu.kobets@gmail.com>
2022-12-22 23:02:55 +0200

    add callorie and user data fields in database schema

--- src/joiSchema/registrationSchema.js
+++ src/joiSchema/registrationSchema.js
@@ -4,7 +4,9 @@ const registrationUserSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string().email().required(),
     password: Joi.string().required().min(6).max(15),
-    token: Joi.string()
+    token: Joi.string(),
+    callorie: Joi.string(),
+    data: Joi.object(),
 });
 
 module.exports = { registrationUserSchema };
