0c23c00d524334bfad78de9b55aac6ef6cf7b835
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-12-23 12:53:36 +0200

    fix email

--- src/joiSchema/authSchema.js
+++ src/joiSchema/authSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const pattern = /(?=.*[A-Za-z])(?=.*[0-9])/;
 const schemaAuth = Joi.object({
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required().min(3).max(254),
+  email: Joi.string().email({ minDomainSegments: 2 }).required().min(3).max(254),
   password: Joi.string().required().min(8).max(100).alphanum().pattern(new RegExp(pattern)),
 });
 
