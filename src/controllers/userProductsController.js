3c79ab772ab6246e07811b730c91da7fefce3248
Yevhenii Kobets <eu.kobets@gmail.com>
2022-12-25 14:48:34 +0200

    fix 400 error

--- src/controllers/userProductsController.js
+++ src/controllers/userProductsController.js
@@ -1,58 +1,52 @@
-const { deleteItemByID } = require("../models/usersProducts");
-const jwt = require("jsonwebtoken");
+const { deleteItemByID } = require('../models/usersProducts');
+const jwt = require('jsonwebtoken');
 
-const {
-  getUserProductsByDate,
-  createUserProduct,
-} = require("../models/usersProducts");
+const { getUserProductsByDate, createUserProduct } = require('../models/usersProducts');
 
 const getUserProductsByDateController = async (req, res, next) => {
-  const date = req.params.date;
+    const date = req.params.date;
 
-  const token = req.headers.authorization.split(" ")[1];
-  const decode = jwt.decode(token);
-  const userId = decode.id;
+    const token = req.headers.authorization.split(' ')[1];
+    const decode = jwt.decode(token);
+    const userId = decode.id;
 
-  // const userId = "1"; // TODO: заменить на user_id из реквеста
-  const usersProducts = await getUserProductsByDate(userId, date);
+    const usersProducts = await getUserProductsByDate(userId, date);
 
-  if (!usersProducts.length) {
-    return res.status(400).json({ message: "No data" });
-  }
+    if (!usersProducts.length) {
+        return res.status(200).json({ message: 'No data' });
+    }
 
-  res.status(200).json(usersProducts);
+    res.status(200).json(usersProducts);
 };
 
 const createUserProductController = async (req, res) => {
-  const date = req.params.date;
-  // const userId = "1"; // TODO: заменить на user_id из реквеста
-  const token = req.headers.authorization.split(" ")[1];
-  const decode = jwt.decode(token);
-  const userId = decode.id;
+    const token = req.headers.authorization.split(' ')[1];
+    const decode = jwt.decode(token);
+    const userId = decode.id;
 
-  const createdUserProduct = await createUserProduct(userId, req.body);
+    const createdUserProduct = await createUserProduct(userId, req.body);
 
-  if (!createdUserProduct) {
-    return res.status(404).json({ message: "Not found" });
-  }
+    if (!createdUserProduct) {
+        return res.status(404).json({ message: 'Not found' });
+    }
 
-  res.status(201).json(createdUserProduct);
+    res.status(201).json(createdUserProduct);
 };
 
 const deleteDiaryListItem = async (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  const item = await deleteItemByID(id);
+    const id = req.params.id;
+    console.log(id);
+    const item = await deleteItemByID(id);
 
-  if (!item) {
-    return res.status(404).json({ message: `id ${id} not found` });
-  }
+    if (!item) {
+        return res.status(404).json({ message: `id ${id} not found` });
+    }
 
-  res.status(200).json({ message: "User successfully deleted", id });
+    res.status(200).json({ message: 'User successfully deleted', id });
 };
 
 module.exports = {
-  getUserProductsByDateController,
-  createUserProductController,
-  deleteDiaryListItem,
+    getUserProductsByDateController,
+    createUserProductController,
+    deleteDiaryListItem,
 };
