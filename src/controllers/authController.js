9bc1ac4d8c1c759e9a78a8df18cba594b663d926
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-25 21:20:09 +0200

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,93 +1,101 @@
-const User = require('../models/schemas/authModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/schemas/authModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { customError } = require('../helpers/errors');
+const { customError } = require("../helpers/errors");
 
 const signup = async (req, res, next) => {
-    const {
-        name,
-        email,
-        password,
-        data = null,
-        callorie = null,
-        notRecommendedProduct = [],
-    } = req.body;
-    const existingEmail = await User.findOne({ email });
-    if (existingEmail) {
-        throw customError({ status: 409, message: 'Email in use' });
-    }
+  const {
+    name,
+    email,
+    password,
+    data = null,
+    callorie = null,
+    notRecommendedProduct = [],
+  } = req.body;
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail) {
+    throw customError({ status: 409, message: "Email in use" });
+  }
 
-    try {
-        const user = new User({ name, email, password, data, callorie, notRecommendedProduct });
-        await user.save();
-
-        const payload = {
-            id: user.id,
-        };
-
-        const token = jwt.sign(payload, process.env.SECRET_KEY);
-
-        await User.findByIdAndUpdate(user.id, { token });
-
-        return res.status(201).json({
-            token,
-            user: {
-                name,
-                email,
-                userId: user._id,
-            },
-        });
-    } catch (error) {
-        next(error);
-    }
-};
-
-const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw customError({ status: 401, message: 'Email or password is wrong' });
-    }
-
-    const passCompare = bcrypt.compareSync(password, user.password);
-    if (!passCompare) {
-        throw customError({ status: 401, message: 'Email or password is wrong' });
-    }
+  try {
+    const user = new User({
+      name,
+      email,
+      password,
+      data,
+      callorie,
+      notRecommendedProduct,
+    });
+    await user.save();
 
     const payload = {
-        id: user.id,
+      id: user.id,
     };
 
     const token = jwt.sign(payload, process.env.SECRET_KEY);
+
     await User.findByIdAndUpdate(user.id, { token });
-    res.status(200).json({
-        token,
-        user: { email: user.email, name: user.name },
+
+    return res.status(201).json({
+      token,
+      user: {
+        name,
+        email,
+        userId: user._id,
+      },
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw customError({ status: 401, message: "Email or password is wrong" });
+  }
+
+  const passCompare = bcrypt.compareSync(password, user.password);
+  if (!passCompare) {
+    throw customError({ status: 401, message: "Email or password is wrong" });
+  }
+
+  const payload = {
+    id: user.id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET_KEY);
+  await User.findByIdAndUpdate(user.id, { token });
+  res.status(200).json({
+    token,
+    user: { email: user.email, name: user.name },
+  });
 };
 
 const logout = async (req, res, next) => {
-    const { id } = req.user;
-    await User.findByIdAndUpdate(id, { token: null });
-    res.status(204).json();
+  const { id } = req.user;
+  await User.findByIdAndUpdate(id, { token: null });
+  res.status(200).json({ message: "Success" });
 };
 
 const currentUser = async (req, res, next) => {
-    const { user } = req;
-    const currentUser = await User.findOne({ token: user.token });
+  console.log("req: ", req);
+  const { user } = req;
+  const currentUser = await User.findOne({ token: user.token });
 
-    return res.status(200).json({
-        user: {
-            name: currentUser.name,
-            email: currentUser.email,
-        },
-    });
+  return res.status(200).json({
+    user: {
+      name: currentUser.name,
+      email: currentUser.email,
+    },
+  });
 };
 
 module.exports = {
-    login,
-    logout,
-    signup,
-    currentUser,
+  login,
+  logout,
+  signup,
+  currentUser,
 };
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, logout, signup, currentUser } = require("../../controllers/authController");
+const {
+  login,
+  logout,
+  signup,
+  currentUser,
+} = require("../../controllers/authController");
 
 const { validationBody } = require("../../middlewares/validationMiddleware.js");
 const { schemaAuth } = require("../../joiSchema/authSchema");
-const { registrationUserSchema } = require("../../joiSchema/registrationSchema");
+const {
+  registrationUserSchema,
+} = require("../../joiSchema/registrationSchema");
 const { asyncWrapper } = require("../../helpers/apiHelpers.js");
 const validationToken = require("../../middlewares/validationToken.js");
 
-router.post("/signup", validationBody(registrationUserSchema), asyncWrapper(signup));
+router.post(
+  "/signup",
+  validationBody(registrationUserSchema),
+  asyncWrapper(signup)
+);
 router.post("/login", validationBody(schemaAuth), asyncWrapper(login));
-router.get("/logout", validationToken, asyncWrapper(logout));
+router.post("/logout", validationToken, asyncWrapper(logout));
 router.get("/current", validationToken, asyncWrapper(currentUser));
 
 module.exports = router;
