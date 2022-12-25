1f444f661c30c9a2c6692f22e5237f840dc2eb3f
Yevhenii Kobets <eu.kobets@gmail.com>
2022-12-26 01:37:51 +0200

    fix route

--- src/controllers/userDiaryInfoController.js
+++ src/controllers/userDiaryInfoController.js
@@ -1,26 +1,35 @@
-const { getUserDiaryInfo, postUserDiaryInfo } = require('../models/diaryInfoModels');
+const {
+  getUserDiaryInfo,
+  postUserDiaryInfo,
+} = require("../models/diaryInfoModels");
 
 const getUserDiaryInfoController = async (req, res, next) => {
-    const { _id } = req.user;
+  const { _id } = req.user;
 
-    const userInfo = await getUserDiaryInfo(_id);
+  const userInfo = await getUserDiaryInfo(_id);
 
-    if (!userInfo) return res.status(400).json({ message: 'No user' });
+  if (!userInfo) return res.status(400).json({ message: "No user" });
 
-    const { callorie, notRecommendedProduct } = userInfo;
+  const { callorie, notRecommendedProduct } = userInfo;
 
-    res.status(200).json({ callorie, notRecommendedProduct });
+  res.status(200).json({ callorie, notRecommendedProduct });
 };
 
 const postUserDiaryInfoController = async (req, res, next) => {
-    const { _id, callorie, notRecommendedProduct } = req.user;
+  const { _id } = req.user;
 
-    await postUserDiaryInfo(_id, callorie, notRecommendedProduct);
+  const { callorie, notRecommendedProduct } = req.body;
 
-    res.status(200).json({ message: 'Succes' });
+  const user = await postUserDiaryInfo(_id, callorie, notRecommendedProduct);
+
+  if (!user) {
+    res.status(404).json("No user");
+  }
+
+  res.status(200).json({ callorie, notRecommendedProduct });
 };
 
 module.exports = {
-    postUserDiaryInfoController,
-    getUserDiaryInfoController,
+  postUserDiaryInfoController,
+  getUserDiaryInfoController,
 };
