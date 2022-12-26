b8e9567387c012b423526db3020dc13afd41fae6
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-12-26 22:24:17 +0200

    shit hapens

--- src/controllers/userProductsController.js
+++ src/controllers/userProductsController.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 const { deleteItemByID } = require("../models/usersProducts");
 const jwt = require("jsonwebtoken");
-=======
-const { deleteItemByID } = require('../models/usersProducts');
-const jwt = require('jsonwebtoken');
->>>>>>> dev
 
-const { getUserProductsByDate, createUserProduct } = require('../models/usersProducts');
+const {
+  getUserProductsByDate,
+  createUserProduct,
+} = require("../models/usersProducts");
 
 const getUserProductsByDateController = async (req, res, next) => {
-<<<<<<< HEAD
   const date = req.params.date;
 
   const token = req.headers.authorization.split(" ")[1];
@@ -17,46 +14,37 @@ const getUserProductsByDateController = async (req, res, next) => {
   const userId = decode.id;
 
   const usersProducts = await getUserProductsByDate(userId, date);
-=======
-    const date = req.params.date;
 
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = jwt.decode(token);
-    const userId = decode.id;
->>>>>>> dev
-
-    const usersProducts = await getUserProductsByDate(userId, date);
-
-    res.status(200).json(usersProducts);
+  res.status(200).json(usersProducts);
 };
 
 const createUserProductController = async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = jwt.decode(token);
-    const userId = decode.id;
+  const token = req.headers.authorization.split(" ")[1];
+  const decode = jwt.decode(token);
+  const userId = decode.id;
 
-    const createdUserProduct = await createUserProduct(userId, req.body);
+  const createdUserProduct = await createUserProduct(userId, req.body);
 
-    if (!createdUserProduct) {
-        return res.status(404).json({ message: 'Not found' });
-    }
+  if (!createdUserProduct) {
+    return res.status(404).json({ message: "Not found" });
+  }
 
-    res.status(201).json(createdUserProduct);
+  res.status(201).json(createdUserProduct);
 };
 
 const deleteDiaryListItem = async (req, res, next) => {
   const id = req.params.id;
   const item = await deleteItemByID(id);
 
-    if (!item) {
-        return res.status(404).json({ message: `id ${id} not found` });
-    }
+  if (!item) {
+    return res.status(404).json({ message: `id ${id} not found` });
+  }
 
   res.status(200).json({ message: "User successfully deleted", id });
 };
 
 module.exports = {
-    getUserProductsByDateController,
-    createUserProductController,
-    deleteDiaryListItem,
+  getUserProductsByDateController,
+  createUserProductController,
+  deleteDiaryListItem,
 };
